f8c32eae98b6503eb4be3a2fc02cbd7ed2b77d90
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-10-18 22:47:08 +0600

    css test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const headerCssInReact = {
+    color: "green",
+  };
 
   return (
     <>
-      
+      <h1 className="headerCss">This is test</h1>
+      <h1 style={headerCssInReact}>This is test</h1>
     </>
   );
 }
 
-export default App
+export default App;
